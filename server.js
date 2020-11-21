da365235f0b6c69332b170d8bd64832a9ae6f163
eshoe2020 <hsulainey@gmail.com>
2020-11-20 20:24:27 -0500

    passport works, database connects

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const port = 3000;
 
 require('dotenv').config()
@@ -13,6 +15,13 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'BYGOD!',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // TODO: Mount routes with app.use()
 
